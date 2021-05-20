977bf53578bd9c263f7109477526430aef9ada37
Filipi Brentegani <filipi.brentegani@gmail.com>
2021-05-20 12:23:02 -0300

    Import Koin in project

---
+++ app/src/main/java/com/filipibrentegani/dependencyinjectionexample/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.filipibrentegani.dependencyinjectionexample
+
+import android.app.Application
+import org.koin.core.context.startKoin
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
